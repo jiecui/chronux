ddf3ccfcc25b0d347e772088c0a9aeb73edfe4a2
Richard Cui <richard.jie.cui@gmail.com>
2018-04-20 16:04:40 -0700

    improved functions ftestc, fitlinesc, rmlinesc for harmonic analysis

--- spectral_analysis/continuous/fitlinesc.m
+++ spectral_analysis/continuous/fitlinesc.m
@@ -1,101 +1,151 @@
-function [datafit,Amps,freqs,Fval,sig]=fitlinesc(data,params,p,plt,f0)
-% fits significant sine waves to data (continuous data).
+function [datafit, Amps, freqs, Fval, sig] = fitlinesc(data, params, p, plt, f0)
+% FITLINESC fits significant sine waves to data (continuous data).
 %
-% Usage: [datafit,Amps,freqs,Fval,sig]=fitlinesc(data,params,p,plt,f0)
+% Syntax:
+%   [datafit, Amps, freqs, Fval, sig] = fitlinesc(data, params, p, plt, f0)
 %
-%  Inputs:  
-% Note that units of Fs, fpass have to be consistent.
-%       data        (data in [N,C] i.e. time x channels/trials or a single
-%       vector) - required.
-%       params      structure containing parameters - params has the
-%       following fields: tapers, Fs, fpass, pad
-%           tapers : precalculated tapers from dpss or in the one of the following
-%                    forms: 
-%                   (1) A numeric vector [TW K] where TW is the
-%                       time-bandwidth product and K is the number of
-%                       tapers to be used (less than or equal to
-%                       2TW-1). 
-%                   (2) A numeric vector [W T p] where W is the
-%                       bandwidth, T is the duration of the data and p 
-%                       is an integer such that 2TW-p tapers are used. In
-%                       this form there is no default i.e. to specify
-%                       the bandwidth, you have to specify T and p as
-%                       well. Note that the units of W and T have to be
-%                       consistent: if W is in Hz, T must be in seconds
-%                       and vice versa. Note that these units must also
-%                       be consistent with the units of params.Fs: W can
-%                       be in Hz if and only if params.Fs is in Hz.
-%                       The default is to use form 1 with TW=3 and K=5
+% Inputs:
+%   data        - (data in [N,C] i.e. time x channels/trials or a single
+%                  vector) - required.
+%   params      - (structure containing parameters) - params has the
+%                 following fields: tapers, Fs, fpass, pad
+%               tapers  : precalculated tapers from dpss or in the one of
+%                         the following forms:
+%                         (1) A numeric vector [TW K] where TW is the
+%                             time-bandwidth product and K is the number of
+%                             tapers to be used (less than or equal to
+%                             2TW-1).
+%                         (2) A numeric vector [W T p] where W is the
+%                             bandwidth, T is the duration of the data and
+%                             p is an integer such that 2TW-p tapers are
+%                             used. In this form there is no default i.e.
+%                             to specify the bandwidth, you have to specify
+%                             T and p as well. Note that the units of W and
+%                             T have to be consistent: if W is in Hz, T
+%                             must be in seconds and vice versa. Note that
+%                             these units must also be consistent with the
+%                             units of params.Fs: W can be in Hz if and
+%                             only if params.Fs is in Hz. The default is to
+%                             use form 1 with TW=3 and K=5
+%               Fs      : (sampling frequency) -- optional. Defaults to 1.
+%               fpass   : (frequency band to be used in the calculation in
+%                         the form [fmin fmax])- optional. Default all
+%                         frequencies between 0 and Fs/2. Note that units
+%                         of Fs and fpass have to be consistent.
+%               pad		: (padding factor for the FFT) - optional (can take
+%                         values -1,0,1,2...). -1 corresponds to no
+%                         padding, 0 corresponds to padding to the next
+%                         highest power of 2 etc. e.g. For N = 500, if PAD
+%                         = -1, we do not pad; if PAD = 0, we pad the FFT
+%                         to 512 points, if pad=1, we pad to 1024 points
+%                         etc. (0 by defaults).
+%   p           - (P-value for F-test) - optional. Defaults set to 0.05.
+%                 The F-distribution has two parameters 2 and 2K-2, where K
+%                 is the number of DPSS (Slepian) tapers.
+%   plt         - (y/n for plot and no plot respectively) - optional.
+%                 Default set to 'n'. If no output assigned, plt = 'y',
+%                 ignoring the user's input of plt.
+%   f0          - frequencies at which you want to remove the lines - if
+%                 unspecified the program uses the f statistic to determine
+%                 appropriate lines.
 %
-%	        Fs 	        (sampling frequency) -- optional. Defaults to 1.
-%               fpass       (frequency band to be used in the calculation in the form
-%                                   [fmin fmax])- optional. 
-%                                   Default all frequencies between 0 and Fs/2
-%	        pad		    (padding factor for the FFT) - optional (can take values -1,0,1,2...). 
-%                    -1 corresponds to no padding, 0 corresponds to padding
-%                    to the next highest power of 2 etc.
-%			      	 e.g. For N = 500, if PAD = -1, we do not pad; if PAD = 0, we pad the FFT
-%			      	 to 512 points, if pad=1, we pad to 1024 points etc.
-%			      	 Defaults to 0.
-%	    p		    (P-value to calculate error bars for) - optional. 
-%                           Defaults to 0.05/N where N is data length.
-%       plt         (y/n for plot and no plot respectively) - plots the
-%       Fratio at all frequencies if y
-%       f0          frequencies at which you want to remove the
-%                   lines - if unspecified the program
-%                   will compute the significant lines
+% Outputs:
+%   datafit     - (linear superposition of fitted sinusoidal waves) the
+%                 signal model is 2*|Aamps|*cos(2*pi*freqs*t+angle(Amps)).
+%   Amps        - (amplitudes at significant frequencies)
+%   freqs       - (significant frequencies)
+%   Fval        - (Fstatistic at all frequencies)
+%   sig         - (significance level for F distribution p value of p)
+
+% Modified by Richard J. Cui.
+% $Revision: 0.2 $  $Date: Fri 04/20/2018 11:13:17.617 AM$
 %
+% 3236 E Chandler Blvd Unit 2036
+% Phoenix, AZ 85048, USA
 %
-%  Outputs: 
-%       datafit        (linear superposition of fitted sine waves)
-%       Amps           (amplitudes at significant frequencies)
-%       freqs          (significant frequencies)
-%       Fval           (Fstatistic at all frequencies)
-%       sig            (significance level for F distribution p value of p)
-data=change_row_to_column(data);
-[N,C]=size(data);
-if nargin < 2 || isempty(params); params=[]; end;
-[tapers,pad,Fs,fpass,err,trialave,params]=getparams(params); 
-clear pad fpass err trialave;
-if nargin < 3 || isempty(p);p=0.05/N;end;
-if nargin < 4 || isempty(plt); plt='n'; end;
-if nargin < 5; f0=[]; end;
-params.tapers=dpsschk(tapers,N,Fs); % calculate the tapers
-[Fval,A,f,sig] = ftestc(data,params,p,plt);
-if isempty(f0);
-   fmax=findpeaks(Fval,sig);
-   freqs=cell(1,C);
-   Amps=cell(1,C);
-   datafit=data;
-   for ch=1:C;
-       fsig=f(fmax(ch).loc);
-       freqs{ch}=fsig;
-       Amps{ch}=A(fmax(ch).loc,ch);
-       Nf=length(fsig);
-%       fprintf('The significant lines for channel %d and the amplitudes are \n',ch);
-%        for nf=1:Nf;
-%            fprintf('%12.8f\n',fsig(nf));
-%            fprintf('%12.8f\n',real(A(fmax(ch).loc(nf),ch)));
-%            fprintf('%12.8f\n',imag(A(fmax(ch).loc(nf),ch))); 
-%            fprintf('\n');
-%        end;
-       datafit(:,ch)=exp(i*2*pi*(0:N-1)'*fsig/Fs)*A(fmax(ch).loc,ch)+exp(-i*2*pi*(0:N-1)'*fsig/Fs)*conj(A(fmax(ch).loc,ch));
-   end;
+% Email: richard.cui@utoronto.ca
+
+% =========================================================================
+% Parse inputs
+% =========================================================================
+data = change_row_to_column(data);
+[N, C] = size(data);
+if nargin < 2 || isempty(params)
+    params=[];
+end
+% [tapers,pad,Fs,fpass,err,trialave,params]=getparams(params);
+% clear pad fpass err trialave;
+[tapers, ~, Fs, ~, ~, ~, params] = getparams(params);
+% if nargin < 3 || isempty(p);p=0.05/N;end;
+if nargin < 3 || isempty(p)
+    p=0.05;
+end
+if nargin < 4 || isempty(plt)
+    plt='n';
+end
+if nargin < 5
+    f0=[];
 else
-   indx = zeros( length(f0) );
-   for n=1:length(f0);
-       [fsig,indx(n)]=min(abs(f-f0(n)));
-   end;
-   fsig=f(indx);
-   for ch=1:C;
-       freqs{ch}=fsig;
-       Amps{ch}=A(indx,ch);
-       Nf=length(fsig);
-%        fprintf('For channel %d the amplitudes and the Fstatistic at f=%f are \n',ch,f0);
-%        fprintf('Fstatistic = %12.8f Fthreshold = %12.8f\n',Fval(indx),sig);
-%        fprintf('Real part of amplitude = %12.8f\n',real(A(indx,ch)));
-%        fprintf('Imaginary part of amplitude = %12.8f\n',imag(A(indx,ch))); 
-       datafit(:,ch)=exp(i*2*pi*(0:N-1)'*fsig/Fs)*A(indx,ch)+exp(-i*2*pi*(0:N-1)'*fsig/Fs)*conj(A(indx,ch));
-   end;
-end;
+    f0 = change_row_to_column(f0);
+end
+if nargout == 0
+    plt = 'y';
+end % if
+
+% =========================================================================
+% Line fit
+% =========================================================================
+params.tapers=dpsschk(tapers,N,Fs); % calculate the tapers
+[Fval, A, f, sig] = ftestc(data, params, p, 'n');
+dfit1ch = @(Amp, freq) 2*real(exp(1i*2*pi*(0:N-1)'*freq/Fs)*Amp);
+
+freqs=cell(1,C);
+Amps=cell(1,C);
+datafit = zeros(size(data));
+if isempty(f0) % if line frequency not specified 
+    fmax=findpeaks(Fval,sig); % chronux function
+    for ch=1:C
+        fsig=f(fmax(ch).loc);
+        freqs{ch}=fsig;
+        Amps{ch} = A(fmax(ch).loc,ch);
+        % datafit(:, ch) = exp(1i*2*pi*(0:N-1)'*fsig/Fs)*Amps{ch}...
+        %     +exp(-1i*2*pi*(0:N-1)'*fsig/Fs)*conj(Amps{ch});
+        datafit(:, ch) = fitplt1ch(dfit1ch, Amps{ch}, freqs{ch}, plt);
+    end
+else % line frequency specified
+    % indx = zeros( length(f0) );
+    indx = zeros(1, length(f0));
+    for n=1:length(f0)
+        [~, indx(n)] = min(abs(f-f0(n))); % find the closest frequency
+    end
+    fsig=f(indx);
+    for ch=1:C
+        freqs{ch}=fsig;
+        Amps{ch}=A(indx,ch);
+        % datafit(:,ch)=exp(1i*2*pi*(0:N-1)'*fsig/Fs)*A(indx,ch)...
+        %     +exp(-1i*2*pi*(0:N-1)'*fsig/Fs)*conj(A(indx,ch));
+        datafit(:, ch) = fitplt1ch(dfit1ch, Amps{ch}, freqs{ch}, plt);
+    end
+end
+
+end % function
+
+% =========================================================================
+% subroutines
+% =========================================================================
+function datafit = fitplt1ch(dfit1ch, A, f, plt)
+
+datafit = dfit1ch(A, f);
+% plot
+if plt == 'y'
+    Nf=length(fsig);
+    fprintf('The significant lines for channel %d (Amplitude, Phase, Frequency)\n',ch)
+    for k=1:Nf
+        fprintf('%8.4f, %8.4f, %8.4f\n', 2*abs(A(k)), angle(A(k)), f(k))
+    end % for
+end % if
+
+end % function
+
+% [EOF]
 
--- spectral_analysis/continuous/ftestc.m
+++ spectral_analysis/continuous/ftestc.m
@@ -1,60 +1,92 @@
-function [Fval,A,f,sig,sd] = ftestc(data,params,p,plt)
-% computes the F-statistic for sine wave in locally-white noise (continuous data).
+function [Fval, A, f, sig, sd] = ftestc(data, params, p, plt)
+% FTESTC computes the F-statistic for sinusoidal wave in locally-white noise (continuous data).
 %
-% [Fval,A,f,sig,sd] = ftestc(data,params,p,plt)
+% Syntax:
+%   [Fval, A, f, sig, sd] = ftestc(data, params, p, plt)
 %
-%  Inputs:  
-%       data        (data in [N,C] i.e. time x channels/trials or a single
-%       vector) - required.
-%       params      structure containing parameters - params has the
-%       following fields: tapers, Fs, fpass, pad
-%           tapers : precalculated tapers from dpss or in the one of the following
-%                    forms: 
-%                    (1) A numeric vector [TW K] where TW is the
-%                        time-bandwidth product and K is the number of
-%                        tapers to be used (less than or equal to
-%                        2TW-1). 
-%                    (2) A numeric vector [W T p] where W is the
-%                        bandwidth, T is the duration of the data and p 
-%                        is an integer such that 2TW-p tapers are used. In
-%                        this form there is no default i.e. to specify
-%                        the bandwidth, you have to specify T and p as
-%                        well. Note that the units of W and T have to be
-%                        consistent: if W is in Hz, T must be in seconds
-%                        and vice versa. Note that these units must also
-%                        be consistent with the units of params.Fs: W can
-%                        be in Hz if and only if params.Fs is in Hz.
-%                        The default is to use form 1 with TW=3 and K=5
+%   data        - (data in [N,C] i.e. time x channels/trials or a single
+%                  vector) - required.
+%   params      - (structure containing parameters) - params has the
+%                 following fields: tapers, Fs, fpass, pad
+%               tapers  : precalculated tapers from dpss or in the one of
+%                         the following forms:
+%                         (1) A numeric vector [TW K] where TW is the
+%                             time-bandwidth product and K is the number of
+%                             tapers to be used (less than or equal to
+%                             2TW-1).
+%                         (2) A numeric vector [W T p] where W is the
+%                             bandwidth, T is the duration of the data and
+%                             p is an integer such that 2TW-p tapers are
+%                             used. In this form there is no default i.e.
+%                             to specify the bandwidth, you have to specify
+%                             T and p as well. Note that the units of W and
+%                             T have to be consistent: if W is in Hz, T
+%                             must be in seconds and vice versa. Note that
+%                             these units must also be consistent with the
+%                             units of params.Fs: W can be in Hz if and
+%                             only if params.Fs is in Hz. The default is to
+%                             use form 1 with TW=3 and K=5
+%               Fs      : (sampling frequency) -- optional. Defaults to 1.
+%               fpass   : (frequency band to be used in the calculation in
+%                         the form [fmin fmax])- optional. Default all
+%                         frequencies between 0 and Fs/2. Note that units
+%                         of Fs and fpass have to be consistent.
+%               pad		: (padding factor for the FFT) - optional (can take
+%                         values -1,0,1,2...). -1 corresponds to no
+%                         padding, 0 corresponds to padding to the next
+%                         highest power of 2 etc. e.g. For N = 500, if PAD
+%                         = -1, we do not pad; if PAD = 0, we pad the FFT
+%                         to 512 points, if pad=1, we pad to 1024 points
+%                         etc. (0 by defaults).
+%   p           - (P-value for F-test) - optional. Defaults set to 0.05.
+%                 The F-distribution has two parameters 2 and 2K-2, where K
+%                 is the number of DPSS (Slepian) tapers.
+%   plt         - (y/n for plot and no plot respectively) - optional.
+%                 Default set to 'n'. If no output assigned, plt = 'y',
+%                 ignoring the user's input of plt.
+% 
+% Outputs: 
+%   Fval        - (F-statistic in frequency x channels/trials format)
+%  	A           - (Line amplitude for X in frequency x channels/trials form) 
+%	f           - (frequencies of evaluation) 
+%   sig         - (F distribution (1-p)% confidence level)
+%   sd          - (standard deviation of the amplitude A)
+
+% Modified by Richard J. Cui.
+% $Revision: 0.2 $  $Date: Fri 04/20/2018  3:33:08.023 PM$
 %
-%	        Fs 	        (sampling frequency) -- optional. Defaults to 1.
-%           fpass       (frequency band to be used in the calculation in the form
-%                                   [fmin fmax])- optional. 
-%                                   Default all frequencies between 0 and Fs/2
-%	        pad		    (padding factor for the FFT) - optional (can take values -1,0,1,2...). 
-%                    -1 corresponds to no padding, 0 corresponds to padding
-%                    to the next highest power of 2 etc.
-%			      	 e.g. For N = 500, if PAD = -1, we do not pad; if PAD = 0, we pad the FFT
-%			      	 to 512 points, if pad=1, we pad to 1024 points etc.
-%			      	 Defaults to 0.
-%	    p		    (P-value to calculate error bars for) - optional. 
-%                           Defaults to 0.05/N where N is the number of samples which
-%	                 corresponds to a false detect probability of approximately 0.05.
-%       plt         (y/n for plot and no plot respectively)
+% 3236 E Chandler Blvd Unit 2036
+% Phoenix, AZ 85048, USA
 %
-%  Outputs: 
-%       Fval        (F-statistic in frequency x channels/trials form)
-%  	    A		    (Line amplitude for X in frequency x channels/trials form) 
-%	    f		    (frequencies of evaluation) 
-%       sig         (F distribution (1-p)% confidence level)
-%       sd          (standard deviation of the amplitude C)
-if nargin < 1; error('Need data'); end;
-if nargin < 2 || isempty(params); params=[]; end;
-[tapers,pad,Fs,fpass,err,trialave,params]=getparams(params);
-clear err trialave
+% Email: richard.cui@utoronto.ca
+
+% =========================================================================
+% Parse inputs
+% =========================================================================
+if nargin < 1
+    error('Need data'); 
+end
+if nargin < 2 || isempty(params)
+    params=[]; 
+end
+if nargin<3 || isempty(p)
+    % p=0.05/N;
+    p = 0.05;
+end
+if nargin<4 || isempty(plt)
+    plt='n';
+end
+if nargout ==0 
+    plt = 'y';
+end % if
+
+% =========================================================================
+% Line F-test
+% =========================================================================
+[tapers,pad,Fs,fpass, ~, ~, params]=getparams(params);
 data=change_row_to_column(data);
 [N,C]=size(data);
-if nargin<3 || isempty(p);p=0.05/N;end;
-if nargin<4 || isempty(plt); plt='n';end;
+
 tapers=dpsschk(tapers,N,Fs); % calculate the tapers
 [N,K]=size(tapers);
 nfft=max(2^(nextpow2(N)+pad),N);% number of points in fft
@@ -71,7 +103,9 @@ J=mtfftc(data,tapers,nfft,Fs);% tapered fft of data - f x K x C
 Jp=J(findx,Kodd,:); % drop the even ffts and restrict fft to specified frequency grid - f x K x C
 tapers=tapers(:,:,ones(1,C)); % add channel indices to the tapers - t x K x C
 H0 = squeeze(sum(tapers(:,Kodd,:),1)); % calculate sum of tapers for even prolates - K x C 
-if C==1;H0=H0';end;
+if C==1
+    H0=H0';
+end
 Nf=length(findx);% number of frequencies
 H0 = H0(:,:,ones(1,Nf)); % add frequency indices to H0 - K x C x f
 H0=permute(H0,[3 1 2]); % permute H0 to get dimensions to match those of Jp - f x K x C 
@@ -86,14 +120,31 @@ Jhat=Ap.*H0; % fitted value for the fft
 num=(K-1).*(abs(A).^2).*squeeze(H0sq);%numerator for F-statistic
 den=squeeze(sum(abs(Jp-Jhat).^2,2)+sum(abs(J(findx,Keven,:)).^2,2));% denominator for F-statistic
 Fval=num./den; % F-statisitic
-if nargout > 3
-   sig=finv(1-p,2,2*K-2); % F-distribution based 1-p% point
-   var=den./(K*squeeze(H0sq)); % variance of amplitude
-   sd=sqrt(var);% standard deviation of amplitude
-end;
-if nargout==0 || strcmp(plt,'y');
-   [S,f]=mtspectrumc(detrend(data),params);subplot(211); plot(f,10*log10(S));xlabel('frequency Hz'); ylabel('Spectrum dB');
-   subplot(212);plot(f,Fval); line(get(gca,'xlim'),[sig sig],'Color','r');xlabel('frequency Hz');
-   ylabel('F ratio');
+sig=finv(1-p,2,2*K-2); % F-distribution (1-p) percential
+var=den./(K*squeeze(H0sq)); % variance of amplitude
+sd=sqrt(var);% standard deviation of amplitude
+
+% plot
+if nargout==0 || strcmp(plt,'y')
+   [S,f]=mtspectrumc(data,params);
+   for k = 1:C
+       figure('Name', sprintf('Channel %d', k))
+       subplot(211)
+       plot(f,10*log10(S(:, k)));
+       set(gca, 'Box', 'Off')
+       xlabel('frequency Hz');
+       ylabel('Spectrum dB');
+       
+       subplot(212)
+       plot(f, Fval(:, k));
+       line(get(gca,'xlim'),[sig sig],'Color','r');
+       set(gca, 'Box', 'Off')
+       xlabel('frequency Hz');
+       ylabel('F ratio');
+   end % for
 end
 A=A*Fs;
+
+end % function
+
+% [EOF]
