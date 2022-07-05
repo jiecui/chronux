9db79caaf4100025ed8f6fc1feb7a9aaf0b98f1b
Richard Jie Cui <richard.cui@utoronto.ca>
2022-07-04 22:30:50 -0500

    cleared the comments

--- spectral_analysis/hybrid/coherencycpb.m
+++ spectral_analysis/hybrid/coherencycpb.m
@@ -27,37 +27,49 @@ function [C, phi, S12, S1, S2, f, zerosp, confC, phistd, Cerr] = coherencycpb(da
     %                               be in Hz if and only if params.Fs is in Hz.
     %                               The default is to use form 1 with TW=3 and K=5
     %                  Fs     : (sampling frequency) - optional. Default 1.
-    %                  fpass  : (frequency band to be used in the calculation 
-    %                           in the form [fmin fmax])- optional. Default 
+    %                  fpass  : (frequency band to be used in the calculation
+    %                           in the form [fmin fmax])- optional. Default
     %                           all frequencies between 0 and Fs/2. Note that
     %                           units of Fs and fpass have to be consistent.
-    %	        pad		    (padding factor for the FFT) - optional (can take values -1,0,1,2...).
-    %                    -1 corresponds to no padding, 0 corresponds to padding
-    %                    to the next highest power of 2 etc.
-    %			      	 e.g. For N = 500, if PAD = -1, we do not pad; if PAD = 0, we pad the FFT
-    %			      	 to 512 points, if pad=1, we pad to 1024 points etc.
-    %			      	 Defaults to 0.
-    %           err  (error calculation [1 p] - Theoretical error bars; [2 p] - Jackknife error bars
-    %                                   [0 p] or 0 - no error bars) - optional. Default 0.
-    %           trialave (average over trials when 1, don't average when 0) - optional. Default 0
-    %       fscorr   (finite size corrections, 0 (don't use finite size corrections) or 1
-    %              (use finite size corrections) - optional (available only for spikes). Defaults 0.
+    %	               pad	  :	(padding factor for the FFT) - optional (can take
+    %                           values -1,0,1,2...). -1 corresponds to no
+    %                           padding, 0 corresponds to padding to the next
+    %                           highest power of 2 etc. e.g. For N = 500, if
+    %                           FPAD = -1, we do not pad; if PAD = 0, we pad
+    %                           the FFT to 512 points, if pad=1, we pad to 1024
+    %                           points etc (Defaults to 0).
+    %                  err    : (error calculation [1 p] - Theoretical error bars;
+    %                           [2 p] - Jackknife error bars; [0 p] or 0 - no
+    %                           error bars) - optional (Default 0).
+    %                  trialave: (average over trials when 1, don't average
+    %                           when 0) - optional. Default 0
+    %   fscorr       - (finite size corrections), 0 (don't use finite size
+    %                  corrections) or 1 (use finite size corrections)
+    %                  - optional (available only for spikes) (Defaults 0).
+    %
     % Outputs:
-    %       C (magnitude of coherency - frequencies x trials if trialave=0; dimension frequencies if trialave=1)
-    %       phi (phase of coherency - frequencies x trials if trialave=0; dimension frequencies if trialave=1)
-    %       S12 (cross spectrum -  frequencies x trials if trialave=0; dimension frequencies if trialave=1)
-    %       S1 (spectrum 1 - frequencies x trials if trialave=0; dimension frequencies if trialave=1)
-    %       S2 (spectrum 2 - frequencies x trials if trialave=0; dimension frequencies if trialave=1)
-    %       f (frequencies)
-    %       zerosp (1 for trials where no spikes were found, 0 otherwise)
-    %       confC (confidence level for C at 1-p %) - only for err(1)>=1
-    %       phistd - theoretical/jackknife (depending on err(1)=1/err(1)=2) standard deviation for phi
-    %                Note that phi + 2 phistd and phi - 2 phistd will give 95% confidence
-    %                bands for phi - only for err(1)>=1
-    %       Cerr  (Jackknife error bars for C - use only for Jackknife -  err(1)=2)
+    %   C           - (magnitude of coherency - frequencies x trials if trialave=0;
+    %                 dimension frequencies if trialave=1)
+    %   phi         - (phase of coherency - frequencies x trials if trialave=0;
+    %                 dimension frequencies if trialave=1)
+    %   S12         - (cross spectrum -  frequencies x trials if trialave=0;
+    %                 dimension frequencies if trialave=1)
+    %   S1          - (spectrum 1 - frequencies x trials if trialave=0;
+    %                 dimension frequencies if trialave=1)
+    %   S2          - (spectrum 2 - frequencies x trials if trialave=0;
+    %                 dimension frequencies if trialave=1)
+    %   f           - (frequencies)
+    %   zerosp      - (1 for trials where no spikes were found, 0 otherwise)
+    %   confC       - (confidence level for C at 1-p %) - only for err(1)>=1
+    %   phistd      - theoretical/jackknife (depending on err(1)=1/err(1)=2)
+    %                 standard deviation for phi.  Note that phi + 2 phistd
+    %                 and phi - 2 phistd will give 95% confidence bands for
+    %                 phi - only for err(1)>=1
+    %   Cerr        - (Jackknife error bars for C - use only for Jackknife
+    %                 -  err(1)=2)
 
     % Modified by Richard J. Cui.
-    % $Revision: 0.1 $  $Date: Tue 06/21/2022  5:25:48.939 PM$
+    % $Revision: 0.2 $  $Date: Mon 07/04/2022  1:01:43.021 PM$
     %
     % Rocky Creek Rd NE
     % Rochester, MN 55906 USA
@@ -117,3 +129,5 @@ function [C, phi, S12, S1, S2, f, zerosp, confC, phistd, Cerr] = coherencycpb(da
     end;
 
     clear Msp2
+
+    % [EOF]
