68f2c5f8395cc8ee86fabc1ffebf3f1dc5019e05
Richard Jie Cui <richard.cui@utoronto.ca>
2022-08-04 00:47:25 -0500

    added comments

--- spectral_analysis/hybrid/coherencycpb.m
+++ spectral_analysis/hybrid/coherencycpb.m
@@ -5,47 +5,47 @@ function [C, phi, S12, S1, S2, f, zerosp, confC, phistd, Cerr] = coherencycpb(da
     %   [C, phi, S12, S1, S2, f, zerosp, confC, phistd, Cerr] = coherencycpb(data1, data2, params, fscorr)
     %
     % Inputs:
-    %   data1        - (continuous data in form samples x trials) -- required
-    %   data2        - (binned spike data in form samples x trials) -- required
-    %   params       - structure with fields: tapers, pad, Fs, fpass, err, trialave
-    %                  -- optional
-    %                  tapers : precalculated tapers from dpss or in the one
-    %                           of the following forms:
-    %                           (1) A numeric vector [TW K] where TW is the
-    %                               time-bandwidth product and K is the number of
-    %                               tapers to be used (less than or equal to
-    %                               2TW-1).
-    %                           (2) A numeric vector [W T p] where W is the
-    %                               bandwidth, T is the duration of the data and p
-    %                               is an integer such that 2TW-p tapers are used. In
-    %                               this form there is no default i.e. to specify
-    %                               the bandwidth, you have to specify T and p as
-    %                               well. Note that the units of W and T have to be
-    %                               consistent: if W is in Hz, T must be in seconds
-    %                               and vice versa. Note that these units must also
-    %                               be consistent with the units of params.Fs: W can
-    %                               be in Hz if and only if params.Fs is in Hz.
-    %                               The default is to use form 1 with TW=3 and K=5
-    %                  Fs     : (sampling frequency) - optional. Default 1.
-    %                  fpass  : (frequency band to be used in the calculation
-    %                           in the form [fmin fmax])- optional. Default
-    %                           all frequencies between 0 and Fs/2. Note that
-    %                           units of Fs and fpass have to be consistent.
-    %	               pad	  :	(padding factor for the FFT) - optional (can take
-    %                           values -1,0,1,2...). -1 corresponds to no
-    %                           padding, 0 corresponds to padding to the next
-    %                           highest power of 2 etc. e.g. For N = 500, if
-    %                           FPAD = -1, we do not pad; if PAD = 0, we pad
-    %                           the FFT to 512 points, if pad=1, we pad to 1024
-    %                           points etc (Defaults to 0).
-    %                  err    : (error calculation [1 p] - Theoretical error bars;
-    %                           [2 p] - Jackknife error bars; [0 p] or 0 - no
-    %                           error bars) - optional (Default 0).
-    %                  trialave: (average over trials when 1, don't average
-    %                           when 0) - optional. Default 0
-    %   fscorr       - (finite size corrections), 0 (don't use finite size
-    %                  corrections) or 1 (use finite size corrections)
-    %                  - optional (available only for spikes) (Defaults 0).
+    %   data1       - (continuous data in form samples x trials) -- required
+    %   data2       - (binned spike data in form samples x trials) -- required
+    %   params      - structure with fields: tapers, pad, Fs, fpass, err, trialave
+    %                 -- optional
+    %                 tapers : precalculated tapers from dpss or in the one
+    %                          of the following forms:
+    %                          (1) A numeric vector [TW K] where TW is the
+    %                              time-bandwidth product and K is the number of
+    %                              tapers to be used (less than or equal to
+    %                              2TW-1).
+    %                          (2) A numeric vector [W T p] where W is the
+    %                              bandwidth, T is the duration of the data and p
+    %                              is an integer such that 2TW-p tapers are used. In
+    %                              this form there is no default i.e. to specify
+    %                              the bandwidth, you have to specify T and p as
+    %                              well. Note that the units of W and T have to be
+    %                              consistent: if W is in Hz, T must be in seconds
+    %                              and vice versa. Note that these units must also
+    %                              be consistent with the units of params.Fs: W can
+    %                              be in Hz if and only if params.Fs is in Hz.
+    %                              The default is to use form 1 with TW=3 and K=5
+    %                 Fs     : (sampling frequency) - optional. Default 1.
+    %                 fpass  : (frequency band to be used in the calculation
+    %                          in the form [fmin fmax])- optional. Default
+    %                          all frequencies between 0 and Fs/2. Note that
+    %                          units of Fs and fpass have to be consistent.
+    %	              pad	  :	(padding factor for the FFT) - optional (can take
+    %                          values -1,0,1,2...). -1 corresponds to no
+    %                          padding, 0 corresponds to padding to the next
+    %                          highest power of 2 etc. e.g. For N = 500, if
+    %                          FPAD = -1, we do not pad; if PAD = 0, we pad
+    %                          the FFT to 512 points, if pad=1, we pad to 1024
+    %                          points etc (Defaults to 0).
+    %                 err    : (error calculation [1 p] - Theoretical error bars;
+    %                          [2 p] - Jackknife error bars; [0 p] or 0 - no
+    %                          error bars) - optional (Default 0).
+    %                 trialave: (average over trials when 1, don't average
+    %                          when 0) - optional. Default 0
+    %   fscorr      - (finite size corrections), 0 (don't use finite size
+    %                 corrections) or 1 (use finite size corrections)
+    %                 - optional (available only for spikes) (Defaults 0).
     %
     % Outputs:
     %   C           - (magnitude of coherency - frequencies x trials if trialave=0;
@@ -130,4 +130,6 @@ function [C, phi, S12, S1, S2, f, zerosp, confC, phistd, Cerr] = coherencycpb(da
 
     clear Msp2
 
-    % [EOF]
+end % function
+
+% [EOF]
