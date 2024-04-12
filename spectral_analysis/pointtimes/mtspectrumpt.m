9fe215fd3007f11fb36b64f50c7008a8e7bb284c
Richard Cui <Cui.Jie@mayo.edu>
2024-04-12 10:57:26 -0500

    Update mtspectrumpt.m with optional frequency parameter

--- spectral_analysis/pointtimes/mtspectrumpt.m
+++ spectral_analysis/pointtimes/mtspectrumpt.m
@@ -5,48 +5,48 @@ function [S, f, R, Serr] = mtspectrumpt(data, params, fscorr, t, options)
     %   [S, f, R, Serr] = mtspectrumpt(data, params, fscorr, t, options)
     %
     % Input(s):
-    %   data        - (structure array of spike times with dimension channels/trials;
-    %                 also accepts 1d array of spike times) -- required
+    %   data        - (structure array of spike times with dimension
+    %                 channels/trials; also accepts 1d array of spike times)
+    %                 -- required
     %   params      - structure with fields tapers, pad, Fs, fpass, err, trialave
     %                 -- optional
     %                 tapers  :precalculated tapers from dpss or in the one
-    %                          of the following forms:
-    %                          (1) A numeric vector [TW K] where TW is the
-    %                              time-bandwidth product and K is the number
-    %                              of tapers to be used (less than or equal
-    %                              to 2TW-1).
-    %                          (2) A numeric vector [W T p] where W is the
-    %                              bandwidth, T is the duration of the data
-    %                              and p is an integer such that 2TW-p
-    %                              tapers are used. In this form there is no
-    %                              default i.e. to specify the bandwidth,
-    %                              you have to specify T and p as well. Note
-    %                              that the units of W and T have to be
-    %                              consistent: if W is in Hz, T must be in
-    %                              seconds and vice versa. Note that these
-    %                              units must also be consistent with the
-    %                              units of params.Fs: W can be in Hz if and
-    %                              only if params.Fs is in Hz. The default
-    %                              is to use form 1 with TW=3 and K=5
+    %                          of the following forms: (1) A numeric vector
+    %                          [TW K] where TW is the time-bandwidth product
+    %                          and K is the number of tapers to be used
+    %                          (less than or equal to 2TW-1). (2) A numeric
+    %                          vector [W T p] where W is the bandwidth, T is
+    %                          the duration of the data and p is an integer
+    %                          such that 2TW-p tapers are used. In this form
+    %                          there is no default i.e. to specify the
+    %                          bandwidth, you have to specify T and p as
+    %                          well. Note that the units of W and T have to
+    %                          be consistent: if W is in Hz, T must be in
+    %                          seconds and vice versa. Note that these units
+    %                          must also be consistent with the units of
+    %                          params.Fs: W can be in Hz if and only if
+    %                          params.Fs is in Hz. -- The default is to use
+    %                          form 1 with TW=3 and K=5.
     %                 pad     :(padding factor for the FFT) - optional (can
     %                          take values -1,0,1,2...). -1 corresponds to
     %                          no padding, 0 corresponds to padding to the
     %                          next highest power of 2 etc. e.g. For N =
     %                          500, if PAD = -1, we do not pad; if PAD = 0,
     %                          we pad the FFT to 512 points, if pad=1, we
-    %                          pad to 1024 points etc. Defaults to 0.
-    %                 Fs      :(sampling frequency) - optional. Default 1.
+    %                          pad to 1024 points etc. -- Defaults to 0.
+    %                 Fs      :(sampling frequency) - optional. -- Default
+    %                          1.
     %                 fpass   :(frequency band to be used in the calculation
-    %                          in the form [fmin fmax])- optional. Default
-    %                          all frequencies between 0 and Fs/2
+    %                          in the form [fmin fmax])- optional. --
+    %                          Default all frequencies between 0 and Fs/2
     %                 err     :(error calculation [1 p] - Theoretical error
     %                          bars; [2 p] - Jackknife error bars [0 p] or 0
-    %                          - no error bars) - optional. Default 0.
+    %                          - no error bars) - optional. -- Default 0.
     %                 trialave:(average over channels/trials when 1, don't
-    %                          average when 0) - optional. Default 0
+    %                          average when 0) - optional. -- Default 0
     %  fscorr       - (finite size corrections, 0 (don't use finite size
     %                 corrections) or 1 (use finite size corrections) --
-    %                 optional (available only for spikes). Defaults 0.
+    %                 optional (available only for spikes). -- Defaults 0.
     %  t            - (time grid over which the tapers are to be calculated:
     %                 this argument is useful when calling the spectrum
     %                 calculation routine from a moving window spectrogram
